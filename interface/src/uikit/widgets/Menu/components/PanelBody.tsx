209abe40f21e923130d3522b4c22774507c9f8d5
Francois de la Rouviere <fassadlr@gmail.com>
2026-03-24 10:17:19 +0000

    Add <a> target to menu item

--- interface/src/uikit/widgets/Menu/components/PanelBody.tsx
+++ interface/src/uikit/widgets/Menu/components/PanelBody.tsx
@@ -96,7 +96,7 @@ function PanelBody({ isPushed, pushNav, isMobile, links } : PanelBodyProps) {
             isActive={entry.href ? isMenuItemActive(entry.href, location.pathname) : false}
             className={calloutClass}
           >
-            <MenuLink href={entry.href} onClick={handleClick}>
+            <MenuLink href={entry.href} onClick={handleClick} target={entry.target}>
               {iconElement}
               <LinkLabel isPushed={isPushed}>{entry.label}</LinkLabel>
               {entry.status && (
