e3c00011b5c1395fb2447e114c49240982d2c0da
Francois de la Rouviere <fassadlr@gmail.com>
2026-04-24 09:45:40 +0100

    Fix token pair sort and cache / Remove DAI base token

--- interface/src/data/Reserves.ts
+++ interface/src/data/Reserves.ts
@@ -27,13 +27,12 @@ export function usePairs(currencies: [Currency | undefined, Currency | undefined
       ]),
     [chainId, currencies]
   )
-
   const pairAddresses = useMemo(
     () =>
       tokens.map(([tokenA, tokenB]) => {
         return tokenA && tokenB && !tokenA.equals(tokenB) ? Pair.getAddress(tokenA, tokenB) : undefined
       }),
-    [tokens]
+    [tokens, chainId]
   )
 
   const results = useMultipleContractSingleData(pairAddresses, PAIR_INTERFACE, 'getReserves')
@@ -43,7 +42,6 @@ export function usePairs(currencies: [Currency | undefined, Currency | undefined
       const { result: reserves, loading } = result
       const tokenA = tokens[i][0]
       const tokenB = tokens[i][1]
-
       if (loading) return [PairState.LOADING, null]
       if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
       if (!reserves) return [PairState.NOT_EXISTS, null]
